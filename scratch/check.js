8d11ee79f465e9826172c9b0ec4a2949852239a0
madymadzz <mohamedradwanali9@gmail.com>
2026-09-19 23:12:38 +0800

    chore: remove accidentally added scratch files

--- scratch/check.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const text = fs.readFileSync('js/pages/orders.js', 'utf8');
-console.log('{', text.split('{').length - 1);
-console.log('}', text.split('}').length - 1);
-console.log('(', text.split('(').length - 1);
-console.log(')', text.split(')').length - 1);
-console.log('`', text.split('`').length - 1);
